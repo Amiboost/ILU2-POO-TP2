75eb8ddbd9d2a99b5a0a0699798e584d01cf1deb
Odin Vasseur--laurens <vsd5319a@ensfsi.onmicrosoft.com>
2026-04-09 16:15:44 +0200

    setup TP2 test

---
+++ test/controleur/ControlEmmenagerTest.java
@@ -0,0 +1,72 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlEmmenagerTest {
+	private Village village;
+	private Chef abraracourcix;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+	}
+
+	@Test
+	void testControlEmmenager() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		assertNotNull(controlEmmenager, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testIsHabitant() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		assertTrue(controlEmmenager.isHabitant("Abraracourcix"));
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		assertTrue(controlEmmenager.isHabitant("Bonemine"));
+		controlEmmenager.ajouterDruide("Panoramix", 10, 1, 5);
+		assertTrue(controlEmmenager.isHabitant("Panoramix"));
+		assertFalse(controlEmmenager.isHabitant("Existe pas"));
+	}
+
+	@Test
+	void testAjouterDruide() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		assertFalse(controlEmmenager.isHabitant("Panoramix"));
+		controlEmmenager.ajouterDruide("Panoramix", 10, 1, 5);
+		assertTrue(controlEmmenager.isHabitant("Panoramix"));
+	}
+
+	@Test
+	void testAjouterGaulois() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		assertFalse(controlEmmenager.isHabitant("Bonemine"));
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		assertTrue(controlEmmenager.isHabitant("Bonemine"));
+		assertFalse(controlEmmenager.isHabitant("PasBonemine"));
+	}
+
+	@Test
+	void testAjouterBeaucoupDeGaulois() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		for (int i = 0; i < 100; i++) {
+			controlEmmenager.ajouterGaulois("GAULOIS_" + i, 10);
+		}
+		for (int i = 0; i < 100; i++) {
+			if (i < 10) {
+				assertTrue(controlEmmenager.isHabitant("GAULOIS_" + i));
+			} else {
+				assertFalse(controlEmmenager.isHabitant("GAULOIS_" + i));
+			}
+		}
+	}
+
+}
